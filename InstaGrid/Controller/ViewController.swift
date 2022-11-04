b57b2161bd0de61e0647947ebf0a12a1b2fe0dbc
cjerent <charlenejerent@gmail.com>
2022-11-04 14:38:57 +0100

    delete button code in main

--- InstaGrid/Controller/ViewController.swift
+++ InstaGrid/Controller/ViewController.swift
@@ -9,21 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var LeftButton: UIButton!
-    
-    @IBOutlet weak var CenterButton: UIButton!
-    
-    @IBOutlet weak var RightButton: UIButton!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        LeftButton.setTitle("", for: .normal)
-//        LeftButton.setImage(UIImage(named: "Layout 1.png"), for: .normal)
-//        CenterButton.setTitle("", for: .normal)
-//        CenterButton.setImage(UIImage(named: "Layout 2.png"), for: .normal)
-//        RightButton.setTitle("", for: .normal)
-//        RightButton.setImage(UIImage(named: "Layout 3.png"), for: .normal)
+
  }
 
     
